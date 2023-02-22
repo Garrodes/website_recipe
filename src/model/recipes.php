5cc71338c57cda05df4101285720f5850cd7cd57
Garrodes <batuque77mlh@gmail.com>
2023-02-22 10:19:24 +0100

    adding files templates/model, problem of recipe

---
+++ src/model/recipes.php
@@ -0,0 +1,8 @@
+<?php 
+// editing class recipe to be extended by classes desert and main_course ?
+// which differences ? is it necesssary to create 3 classes or just 1 ? 
+// which one is more efficient ?
+// also need to display different pizzas or pies or cakes
+// in the db differentiate them ? 
+// but they are the same object ? 
+// cost of selecting them all every time ?
