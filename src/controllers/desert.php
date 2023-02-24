dd2ffae1c18896e81f5537c2c66a9755109a5ce8
Garrodes <batuque77mlh@gmail.com>
2023-02-24 14:34:14 +0100

    adding spr_autoload_register, passage comment

--- src/controllers/desert.php
+++ src/controllers/desert.php
@@ -1,9 +1,10 @@
 <?php
-
+spl_autoload_register();
+/* passage en commentaire pour tester spl_autoload_register()
 require_once('src/lib/database.php');
 require_once('src/model/desert.php');
 
-use App\Model\desert\DesertRepository;
+use App\Model\desert\DesertRepository; */
 
 function desert(string $identifier)
     {
