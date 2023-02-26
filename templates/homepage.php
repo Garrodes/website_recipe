dc8e7e4ab498f37d79eb2e9ee01db434ad73beea
Garrodes <batuque77mlh@gmail.com>
2023-02-26 17:06:36 +0100

    meme prob que blog, pas de recup id site du vent

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,5 +1,11 @@
 <?php $title="Site de Recette Accueil" ?>
+
 <?php ob_start(); ?>
+
 <h1> Accueil du Site </h1>
 
+<h2><a href="index.php?action=desert=<?= urlencode($desert-> identifier)?>"> Desserts </a></h2>
+
 <?php $content=ob_get_clean(); ?>
+
+<?php require('templates/layout.php'); ?>
