13dd3ee81c0a74f0231f4164f7210ed51f451c64
Garrodes <batuque77mlh@gmail.com>
2023-03-09 14:17:37 +0100

    writing sql queries

--- src/model/recipes.php
+++ src/model/recipes.php
@@ -60,21 +60,27 @@ class RecipeRepository
             ( " INSERT INTO recipe(recipe_name, recipe_order) VALUES (?, ?)");
             // preventing sql attack ??
            
-            $image = $statement -> execute([
+            $image1= $statement -> execute([
                 $recipe_name, $recipe_order
             ]);
 
-            $statement2 = $this -> connection -> getConnection() -> prepare 
-            ( " WITH fictive AS ( 
-            SELECT recipe_id, ing_id FROM recipe,ing WHERE 
             
-            
-            
-            INSERT INTO recipe_ing(recipe_id, ing_id, q, unit) VALUES 
-            (recipe_id, SELECT recipe_id FROM recipe WHERE :recipe_name),
-            (ing_id, SELECT ing_id FROM ing WHERE :ing_name),
-            ?, ?");
+            $id_recipe_stmt = $this -> connection -> getConnection() -> prepare 
+            ("SELECT recipe_id FROM recipe WHERE recipe_name = :Valid_recipe_name");
+            $id_recipe_stmt = bindParam(':Valid_recipe_name', $recipe_name);
+            $addedrecipe_id = $id_recipe_stmt -> execute([':Valid_recipe_name'=> $recipe_name]);
 
+            $id_ing_stmt = $this -> connection -> getConnection() -> prepare
+            ("SELECT ing_id FROM ing WHERE ing_name = :Valid_ing_name");
+            $id_ing_stmt = bindParam(':Valid_ing_name', $ing_name);
+            $addeding_id = $id_ing_stmt -> execute ([':Valid_ing_name', $ing_name]);
+
+            $statement2 = $this -> connection -> getConnection() -> prepare 
+            ( " INSERT INTO recipe_ing(recipe_id, ing_id, q, L) VALUES (?,?,?,?)");
+            $image2 = $statement2 -> execute([$addedrecipe_id, $addeding_id, $q, $L]);
+
+            $image[] = ([$image1, $image2]);
+            
             return ($image >0 );
     
          
